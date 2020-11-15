470b0c38c7bc0340f4e28162a0fb12b94957f36f
Agnieszka <agnieszka.lasinkiewicz@yahoo.com>
2020-11-15 18:12:59 +0100

    whatever

---
+++ case-converter.js
@@ -0,0 +1,21 @@
+
+
+        function capitalizedCase() {
+
+            var firstInput = document.getElementById('titleInput').value.trim().toLowerCase().split(' ');
+            for(var i = 0;i < firstInput.length;i++) {
+                firstInput[i] = firstInput[i].charAt(0).toUpperCase() + firstInput[i].slice(1); 
+                document.getElementById('result').innerHTML = firstInput.join(' ');
+            }         
+        }            
+            
+        function urlCase() {
+            document.getElementById('result').innerHTML = document.getElementById('titleInput').value.toLowerCase().trim().replace('&','and').replace(/ś/ig, 's').replace(/ń/ig, 'n').replace(/ć/ig, 'c').replace(/ź/ig, 'z').replace(/ż/ig, 'z').replace(/ą/ig,'a').replace(/ę/ig,'e').replace(/ó/ig,'o').replace(/ł/ig,'l').replace('@', 'at').replace('#', 'number-').replace('£', 'pound').replace(/ /g, '-').replace(/–|\|/,'').replace('--','-').replace('---','-').replace(/^-+|-+$|:|;|,|\.|'|"|\?|\!|\(|\)/gi,'');  
+
+        }
+        
+        function copyText() {
+            document.getElementById('result').select();
+            document.execCommand("copy");
+
+        }
